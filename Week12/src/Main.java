39546d9d7b4a75611d607a043fd03267cb82bca1
HaniElhamwi <hanialhamwi000000@gmail.com>
2023-12-30 18:46:30 +0300

    D

---
+++ Week12/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>(
+                Arrays.asList(3, 7, 9, 2, 5, 5, 8, 5, 6, 3, 4, 7, 3, 1)
+        );
+
+        System.out.println(list);
+        System.out.println(removeBadPairs(list));
+    }
+
+    public static ArrayList<Integer> removeBadPairs(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i += 2) {
+            if (list.get(i) > list.get(i + 1)) {
+                list.remove(i);
+                list.remove(i);
+                i -= 2;
+            }
+        }
+
+        return list;
+    }
+}
